db5b296a8f31bd20e9976dc1a2923924325b178b
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2025-02-16 18:44:35 +0500

    Db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+const dbconnect = async () => {
+  const connection = await mongoose
+    .connect(
+      "mongodb+srv://dawood:03348757377d@cluster0.nlusybm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => {
+      console.log("connected to database");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to database", err);
+    });
+};
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+dbconnect().then(() => {
+  app.get("/", (req, res) => {
+    res.send("Server is running");
+  });
+
+  app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+  });
 });
