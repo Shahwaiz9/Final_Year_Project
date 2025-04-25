822863a9cfa933f1d0f8adb62e1301a1b7f11e01
Dawood Nadeem <80353999+dawood23@users.noreply.github.com>
2025-04-26 00:15:15 +0500

    order-route

--- Backend/routes/orderRouter.js
+++ Backend/routes/orderRouter.js
@@ -92,6 +92,18 @@ router.get("/user-orders", Authenticated, async (req, res) => {
   }
 });
 
+router.get("/:orderid", Authenticated, async (req, res) => {
+  try {
+    const order = await Order.find({ _id: req.query.orderid })
+      .populate("product")
+      .populate("vendor", "CompanyName");
+
+    res.status(200).json({ success: true, order });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
 router.get("/vendor-orders", Authenticated, async (req, res) => {
   try {
     if (req.user.role !== "vendor") {
